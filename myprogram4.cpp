0cbd285a294a4399b38a379e72bccb7b29ff15ea
liya4545 <liyashahana9@gmail.com>
2026-07-08 23:18:53 +0530

    Add files via upload

---
+++ myprogram4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cin >> age;
+    if(age>=18){
+        cout << "an adult";
+        
+    }
+    else{
+        cout << "not an adult";
+        return 0;
+    }
+
+}
